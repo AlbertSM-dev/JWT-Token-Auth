aac8d6fb05042094c700bed1d1f941848628dfe3
AlbertSM-dev <lokdok1988@hotmail.com>
2024-05-06 20:33:17 +0200

    Add project files.

---
+++ WebAPI/Controllers/LoginController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoginController : ControllerBase
+    {
+        private IConfiguration _config;
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] LoginRequest loginRequest)
+        {
+            //your logic for login process
+            //If login usrename and password are correct then proceed to generate token
+            if (loginRequest is null)
+            {
+                return BadRequest("Invalid Email/Password!");
+            }
+            if (loginRequest.Email == "a@a.com" && loginRequest.Password == "Pass@1234")
+            {
+                var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+                var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+                var Sectoken = new JwtSecurityToken(_config["Jwt:Issuer"],
+                  _config["Jwt:Issuer"],
+                  null,
+                  expires: DateTime.Now.AddMinutes(120),
+                  signingCredentials: credentials);
+
+                var token = new JwtSecurityTokenHandler().WriteToken(Sectoken);
+
+                return Ok(token);
+            }
+            return Unauthorized();
+        }
+    }
+}
